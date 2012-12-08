b1ab05ed58636d4a07c0febecdf24aa007efbde1
oneEyedOdin <riderj51+github@gmail.com>
2012-12-08 15:57:02 -0600

    Basic JS functions.

--- options.js
+++ options.js
@@ -1,3 +1,23 @@
 function limit(){
 
 }
+
+function addURL(){
+
+}
+
+function saveURLs(){
+
+}
+
+function addHTTP(urlString){
+
+	//Check for http
+	//alert(urlString);
+	
+	validateURL(urlString);
+}
+
+function validateURL(urlString){
+
+}
