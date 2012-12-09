7c6df9d99d5634eb5ccd9163220e5fab55cb6787
oneEyedOdin <riderj51+github@gmail.com>
2012-12-09 14:21:05 -0600

    Saving of URL settings.

--- options.js
+++ options.js
@@ -3,21 +3,50 @@ function limit(){
 }
 
 function addURL(){
-
+	$("#urlForm").append('<br/>');
+	$("#urlForm").append('<div><label>URL</label><input type="text" id="urlInput"/>'+
+						 '<label id="numberLabel">Limit</label><input type="number" id="urlLimit" min="1"/>'+
+	                     '<select id="timeLimit"><option name="Hourly">Hourly</option>'+
+	                     '<option name="Daily">Daily</option><option name="Weekly">Weekly</option></select>');
 }
 
+/*function removeURL(elementToRemove){
+	$("#urlForm").remove(elementToRemove.id);
+}
+*/
 function saveURLs(){
 
-}
-
-function addHTTP(urlString){
-
-	//Check for http
-	//alert(urlString);
+	var urlList = new Array();
 	
-	validateURL(urlString);
+	$("#urlList").children('div').each(function(i){
+		var url = $(this).children("#urlInput").val();
+		var limit = parseInt($(this).children("#urlLimit").val());
+		var timeLimit = $(this).children("#timeLimit").val();
+		
+		//alert(url + " " + limit + " " + timeLimit);
+		
+		if(url.length > 0 && limit > 0 && timeLimit.length > 0){
+			var limitObj = new URL_Limit(url, limit, timeLimit);
+			urlList[i] = limitObj.toString();
+		}
+	});
+	
+	/*
+	for(var i=0; i < urlList.length; i++){
+		alert(urlList[i].toString());
+	}
+	*/
+	
+	localStorage['urlLimits'] = JSON.stringify(urlList);
 }
 
-function validateURL(urlString){
-
+/** URL Limit Object **/
+function URL_Limit(url, limit, timeLimit){
+  this.url = url;
+  this.limit = limit;
+  this.timeLimit = timeLimit;
+  
+  this.toString = function(){
+	return this.url + "," + this.limit + "," + this.timeLimit;
+  }
 }
