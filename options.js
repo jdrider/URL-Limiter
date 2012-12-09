fc1114125ead557aeb061dd5eadf616641eb9d41
oneEyedOdin <riderj51+github@gmail.com>
2012-12-09 15:15:54 -0600

    Load URL functionality.

--- options.js
+++ options.js
@@ -5,7 +5,7 @@ function limit(){
 function addURL(){
 	$("#urlList").append('<br/>');
 	$("#urlList").append('<div><label>URL</label><input type="text" id="urlInput"/>'+
-						 '<label id="numberLabel">Limit</label><input type="number" id="urlLimit" min="1"/>'+
+						 '<label id="numberLabel">Limit</label><input type="number" id="urlLimit" min="1" value="1"/>'+
 	                     '<select id="timeLimit"><option name="Hourly">Hourly</option>'+
 	                     '<option name="Daily">Daily</option><option name="Weekly">Weekly</option></select>');
 }
@@ -14,16 +14,42 @@ function addURL(){
 	$("#urlForm").remove(elementToRemove.id);
 }
 */
+
+function loadURLs(){
+	var urls = localStorage['urlLimits'];
+	
+	if(urls != undefined){
+	
+		var urlList = JSON.parse(urls);
+		
+		if(urlList != undefined){
+			
+			$("#urlList").html("");
+			
+			for(var i = 0; i < urlList.length; i++){
+				
+				var limitItem = createURL_Limit(urlList[i]);
+				
+				if(i != 0){
+					$("#urlList").append('<br/>');
+				}
+				$("#urlList").append('<div><label>URL</label><input type="text" id="urlInput" value="'+ limitItem.url + '"/>'+
+						             '<label id="numberLabel">Limit</label><input type="number" id="urlLimit" min="1" value="'+ limitItem.limit +'"/>'+
+	                                 '<select id="timeLimit" value="'+ limitItem.timeLimit +'"><option name="Hourly">Hourly</option>'+
+	                                 '<option name="Daily">Daily</option><option name="Weekly">Weekly</option></select>');
+			}
+		}
+	}
+}
+
 function saveURLs(){
 
 	var urlList = new Array();
 	
 	$("#urlList").children('div').each(function(i){
 		var url = $(this).children("#urlInput").val();
-		var limit = parseInt($(this).children("#urlLimit").val());
+		var limit = $(this).children("#urlLimit").val();
 		var timeLimit = $(this).children("#timeLimit").val();
-		
-		//alert(url + " " + limit + " " + timeLimit);
 		
 		if(url.length > 0 && limit > 0 && timeLimit.length > 0){
 			var limitObj = new URL_Limit(url, limit, timeLimit);
@@ -31,21 +57,21 @@ function saveURLs(){
 		}
 	});
 	
-	/*
-	for(var i=0; i < urlList.length; i++){
-		alert(urlList[i].toString());
-	}
-	*/
-	
 	if(urlList.length > 0){
 		localStorage['urlLimits'] = JSON.stringify(urlList);
 	}
 }
 
+function createURL_Limit(valueString){
+	var indValues = valueString.split(",");
+	
+	return new URL_Limit(indValues[0], indValues[1], indValues[2]);
+}
+
 /** URL Limit Object **/
 function URL_Limit(url, limit, timeLimit){
   this.url = url;
-  this.limit = limit;
+  this.limit = parseInt(limit);
   this.timeLimit = timeLimit;
   
   this.toString = function(){
